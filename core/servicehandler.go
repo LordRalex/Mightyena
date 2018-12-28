32483fdbcd5e233d09c27814c786d0ee398c5035
Joshua Taylor <lordralex@gmail.com>
2018-12-28 16:23:04 -0600

    Add debug info

--- core/servicehandler.go
+++ core/servicehandler.go
@@ -13,6 +13,8 @@ func CreateServiceHandlers(connection *irc.Connection) {
 }
 
 func handleJoin(event *irc.Event) {
+	//[SERVICE] [JOIN] &{Code:JOIN Raw::Mightyena!~Bot@beast.of.the.devilincarnate.net JOIN #minecrafthelp Nick:Mightyena Host:beast.of.the.devilincarnate.net Source:Mightyena!~Bot@beast.of.the.devilincarnate.net User:~Bot Arguments:[#minecrafthelp] Tags:map[] Connection:0xc0001aa000 Ctx:context.Background}
+	//[SERVICE] [JOIN] &{Code:JOIN Raw::Lord_Ralex!~Ralex@irc2.ae97.net JOIN #minecrafthelp Nick:Lord_Ralex Host:irc2.ae97.net Source:Lord_Ralex!~Ralex@irc2.ae97.net User:~Ralex Arguments:[#minecrafthelp] Tags:map[] Connection:0xc0001aa000 Ctx:context.Background}
 	fmt.Printf("[SERVICE] [JOIN] %+v\n", event)
 }
 
@@ -21,6 +23,7 @@ func handleQuit(event *irc.Event) {
 }
 
 func handlePart(event *irc.Event) {
+	//[SERVICE] [PART] &{Code:PART Raw::Lord_Ralex!~Ralex@irc2.ae97.net PART #minecrafthelp :Leaving Nick:Lord_Ralex Host:irc2.ae97.net Source:Lord_Ralex!~Ralex@irc2.ae97.net User:~Ralex Arguments:[#minecrafthelp Leaving] Tags:map[] Connection:0xc0001aa000 Ctx:context.Background}
 	fmt.Printf("[SERVICE] [PART] %+v\n", event)
 }
 
