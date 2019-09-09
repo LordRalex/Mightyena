c3dead63e0fadde2894589bb274cc7faa7881f65
Joshua Taylor <lordralex@gmail.com>
2019-09-09 12:55:41 -0500

    Use 004 which is standard

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func main() {
 		return
 	}
 
-	bot.AddCallback("376", func(e *irc.Event) {
+	bot.AddCallback("004", func(e *irc.Event) {
 		nickserv, _ := coreConfig.GetString("nickserv")
 		pw, _ := coreConfig.GetString("password")
 
